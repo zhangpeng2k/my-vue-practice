9a5ec2442580416def03959be913241a2ba678c6
zhangpeng2k <1028158677@qq.com>
2019-09-03 15:45:00 +0800

    my-vue-router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+// import Router from 'vue-router'
+import Router from './my-vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+
 
 Vue.use(Router)
 
@@ -14,15 +17,20 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     },
-    {
-      path: '/form',
-      name: 'form',
-      component: () => import('@/components/form')
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
+    // {
+    //   path: '/form',
+    //   name: 'form',
+    //   component: () => import('@/components/form')
+    // },
   ]
 })
